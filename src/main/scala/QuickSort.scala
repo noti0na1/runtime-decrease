d5f6a57c34871b0dba08b98dc6231b82f95d872f
theMaimu <barmettler.lars@gmail.com>
2023-12-12 09:57:09 +0100

    refactor quicksort

--- src/main/scala/QuickSort.scala
+++ src/main/scala/QuickSort.scala
@@ -7,31 +7,26 @@ object QuickSort {
     }
   }
 
-  def appendSorted(l1: List[BigInt], l2: List[BigInt]): List[BigInt] = {
-    l1 match {
-      case Nil => l2
-      case x::xs => x +: appendSorted(xs, l2)
-    }
-  } 
-
   def quickSort(list: List[BigInt])(using
       DecreaseState
   ): List[BigInt] = {
-    decreases(list.size)
-    list match {
-      case Nil => Nil
-      case x::xs => par(x, Nil, Nil, xs)
+    decreases(list.size) {
+        list match {
+        case Nil => Nil
+        case x::xs => par(x, Nil, Nil, xs)
+        }
     }
   }
 
   def par(x: BigInt, l: List[BigInt], r: List[BigInt], ls: List[BigInt])(using
       DecreaseState
   ): List[BigInt] = {
-    decreases(l.size + r.size + ls.size, ls.size + 1)
-
-    ls match {
-      case Nil => appendSorted(quickSort(l), x +: quickSort(r))
-      case x2::xs2 => if (x2 <= x) par(x, x2 +: l, r, xs2) else par(x, l, x2 +: r, xs2)
-    }
+    decreases(l.size + r.size + ls.size, ls.size + 1) {
+        ls match {
+            case Nil => quickSort(l) ++ (x +: quickSort(r))
+            case x2::xs2 => if (x2 <= x) par(x, x2 +: l, r, xs2) else par(x, l, x2 +: r, xs2)
+        }
+    } 
   }
+
 }
